0c518198f55cff6b6ccdc3cf9ddee117f5121da5
masahiro yasunaga <yasunaga37@gmail.com>
2018-09-01 16:23:24 +0900

    Add DestroyServlet.java

--- message_board/WebContent/WEB-INF/views/messages/edit.jsp
+++ message_board/WebContent/WEB-INF/views/messages/edit.jsp
@@ -1,6 +1,14 @@
 <%@ page language="java" contentType="text/html; charset=UTF-8" pageEncoding="UTF-8"%>
 <%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
 
+<script>
+	function confirmDestry() {
+		if (confirm("本当に削除してよろしいですか？")) {
+			document.forms[1].submit();
+		}
+	}
+</script>
+
 <c:import url="../layout/app.jsp">
 	<c:param name="content">
 		<h2>id：${message.id} のメッセージ編集ページ</h2>
@@ -10,5 +18,11 @@
 		</form>
 
 		<p><a href="<c:url value='/index' />">一覧に戻る</a></p>
+		<p><a href="#" onclick="confirmDestry();">このメッセージを削除する</a></p>
+
+		<form method="post" action="<c:url value='/destroy' />">
+			<input type="hidden" name="_token" value="${_token}" />
+		</form>
+
 	</c:param>
 </c:import>
---
+++ message_board/src/controllers/DestroyServlet.java
@@ -0,0 +1,49 @@
+package controllers;
+
+import java.io.IOException;
+
+import javax.persistence.EntityManager;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import models.Message;
+import utils.DBUtil;
+
+/**
+ * Servlet implementation class DestroyServlet
+ */
+@WebServlet("/destroy")
+public class DestroyServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public DestroyServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String _token = request.getParameter("_token");
+		HttpSession session = request.getSession();
+		if (_token != null && _token.equals(session.getId())) {
+			EntityManager em = DBUtil.createEntityManager();
+			Message m = em.find(Message.class, (Integer)(session.getAttribute("message_id")));
+			em.getTransaction().begin();
+			em.remove(m);
+			em.getTransaction().commit();
+			em.close();
+			session.removeAttribute("message_id");
+			response.sendRedirect(request.getContextPath() + "/index");
+		}
+	}
+
+}
